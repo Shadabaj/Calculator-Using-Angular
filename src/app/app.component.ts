45b53b3be3776fb536af6f282352444947279045
Shadab Ansari <shadab.ansari@idsnext.com>
2023-10-16 23:02:29 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculator';
+  toshow='0';
+current_value=''
+  writetoinput(value:string){
+this.current_value=this.current_value+value;
+this.toshow=this.current_value;
+  }
+
+  equals(){
+    this.toshow=eval(this.current_value);
+    this.current_value=this.toshow;
+  }
+
+  clear(){
+    this.current_value='';
+    this.toshow='0';
+  }
+
+  back(){
+    this.current_value=this.current_value.slice(0,-1);
+    this.toshow=this.current_value;
+    if(this.current_value==''){
+      this.toshow='0';
+
+    }
+  }
+
+  calcvalue(solve:any){
+    if(solve.charAt(0)=='0')
+    {
+      solve=solve.slice(1,);
+    }
+    this.toshow=eval(solve);
+  }
 }
